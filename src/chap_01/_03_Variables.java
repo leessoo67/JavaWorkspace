7241b66802783d2e064dab90c00f4e2246072abc
이수현 < >
2024-09-23 16:09:12 +0900

    3 test

--- src/chap_01/_03_Variables.java
+++ src/chap_01/_03_Variables.java
@@ -5,5 +5,6 @@ public class _03_Variables {
         System.out.println("s나도코딩님, 배송이 시작됩니다. 15시에 방문 예정입니다.");
         System.out.println("나도코딩님, 배송이 완료되엇습니다.");
         //test
+        //test2
     }
 }
