a32985dda9087f93151d6aad3562f48204b09b89
이수현 < >
2024-09-23 15:55:57 +0900

    th commit

--- src/chap_01/_03_Variables.java
+++ src/chap_01/_03_Variables.java
@@ -4,5 +4,6 @@ public class _03_Variables {
     public static void main(String[] args) {
         System.out.println("s나도코딩님, 배송이 시작됩니다. 15시에 방문 예정입니다.");
         System.out.println("나도코딩님, 배송이 완료되엇습니다.");
+        //test
     }
 }
